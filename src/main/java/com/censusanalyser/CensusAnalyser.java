879afdbea19e96dbd35bca5620c106d240d51930
santospanda111 <santospanda111@gmail.com>
2021-05-25 14:33:53 +0530

    Added welcome message

---
+++ src/main/java/com/censusanalyser/CensusAnalyser.java
@@ -0,0 +1,11 @@
+package com.censusanalyser;
+
+public class CensusAnalyser {
+    /**
+     * Added welcome message.
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian State Census Analyser");
+    }
+}
